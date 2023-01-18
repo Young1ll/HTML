fc0fbe8fe2f66743150200cea3c47e07a7d751d6
Young1ll <goooglecho01@gmail.com>
2023-01-18 18:03:40 +0900

    Reset

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-async function fetchHtmlAsText(url) {
-    return await (await fetch(url)).text();
-}
+document.addEventListener("DOMContentLoaded", () => {
+    
+    async function fetchHtmlAsText(url) {
+        return await (await fetch(url)).text();
+    }
+    
+    async function importPage(target) {
+        document.querySelector('#' + target).innerHTML = await fetchHtmlAsText(target + '.html');
+    }
+    
+    importPage('introduction');
 
-async function importPage(target) {
-    document.querySelector('#' + target).innerHTML = await fetchHtmlAsText(target + '.html');
-}
+});
 
-importPage('introduction');
+
